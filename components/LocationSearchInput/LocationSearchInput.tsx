4f62059c060a60425d7643f2eed9d82ae7c27eeb
Peter Ng <kachonng1@gmail.com>
2024-07-22 19:55:18 +0900

    update loaction search input component to input

--- components/LocationSearchInput/LocationSearchInput.tsx
+++ components/LocationSearchInput/LocationSearchInput.tsx
@@ -9,9 +9,19 @@ const LocationSearchInput = ({
   country: string;
 }) => {
   return (
-    <div className="flex w-full justify-between rounded-full bg-gray-200 p-4 px-6">
-      {region}, {country}
-      <Image src={searchIcon} alt="Search" height={20} width={20} />
+    <div className="flex w-full min-w-80 justify-between rounded-full bg-gray-200 p-4 px-6">
+      <input
+        type="text"
+        className="w-10/12 truncate bg-gray-200 outline-none"
+        defaultValue={`${region}, ${country}`}
+      />
+      <Image
+        src={searchIcon}
+        alt="Search"
+        height={20}
+        width={20}
+        className="cursor-pointer"
+      />
     </div>
   );
 };
